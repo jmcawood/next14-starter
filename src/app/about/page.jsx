18c370914dbadb34b85899e177fa46f589d9c3b4
jmcawood10 <jmcawood@hotmail.com>
2024-08-25 19:31:02 +1000

    Up to 1:34:50

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,45 @@
 import Image from "next/image"
+import styles from "./about.module.css"
 
 const AboutPage = () => {
     return (
-        <Image src="/about.png" alt="" width={500} height={500}/>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>
+                    We create digital ideas that are bigger, bolder, braver and better.
+                </h1>
+                <p className={styles.description}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ratione ducimus incidunt voluptatibus voluptatum maxime nulla 
+                    quis minima nostrum ea labore optio, eos, illo rerum perferendis delectus laboriosam quia modi aperiam error sequi! 
+                    Architecto reprehenderit voluptate nostrum distinctio esse modi nihil. Distinctio pariatur doloribus et, eaque nulla magnam modi! Odit.
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image
+                src="/about.png"
+                alt="About image"
+                fill
+                className={styles.img}
+                />
+            </div>
+        </div>
     )
 }
 
 export default AboutPage
+
---
+++ src/components/postCard/postCard.jsx
@@ -0,0 +1,23 @@
+import styles from "./postCard.module.css"
+import Image from "next/image";
+import Link from "next/link";
+
+const PostCard = () => {
+     return (
+         <div className={styles.container}>
+            <div className={styles.top}>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/26653530/pexels-photo-26653530/free-photo-of-stars-streaks-over-snow-covered-mountain.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img}/>
+                </div>
+                <span className={styles.date}>25-08-2024</span>
+            </div>
+            <div className={styles.bottom}>
+                <h1 className={styles.title}>Title</h1>
+                <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto vel ratione cupiditate magni autem consectetur perspiciatis maiores non enim incidunt!</p>
+                <Link className={styles.link} href="/blog/post">READ MORE</Link>
+            </div>
+         </div>
+     )
+};
+
+export default  PostCard;
